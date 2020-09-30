df9ae5a54b3000c3e394dc88482b6070e8533b2c
AlexeyIzhbulatov <ouren@mail.ru>
2020-09-30 00:20:16 -0700

    KanBan 1/4

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
 import React, {useState} from 'react';
 import './App.css';
-import { v4 as uuidv4 } from 'uuid';
-import ColumnTask from "./ColumnTask";
+import {v4 as uuidv4} from 'uuid';
+import Board from "./Board";
 
-
-const taskList = [
-    {id: uuidv4(), nameTask: 'Task1', priority: 0, taskGenetation: 'Good job jo next man!', status: 'todo'},
-    {id: uuidv4(), nameTask: 'Task2', priority: 1, taskGenetation: 'Good job jo next man!', status: 'review'},
-    {id: uuidv4(), nameTask: 'Task3', priority: 2, taskGenetation: 'Good job jo next man!', status: 'progress'},
-    {id: uuidv4(), nameTask: 'Task4', priority: 2, taskGenetation: 'Good job jo next man!', status: 'done'},
+const initialTasks = [
+    {id: uuidv4(), title: 'Task1', priority: 0, status: 'todo'},
+    {id: uuidv4(), title: 'Task2', priority: 1, status: 'progress'},
+    {id: uuidv4(), title: 'Task3', priority: 2, status: 'review'},
+    {id: uuidv4(), title: 'Task4', priority: 2, status: 'done'},
 ]
 
 
 function App() {
 
-    const [tasks, setTasks] = useState(taskList);
+    const [task, setTask] = useState(initialTasks)
 
+    const leftArrow = (id, status) => {
+        console.log(id, status)
+    }
 
-  return (
-    <div className="App">
-        <div className="container">
-            Kanban
-            <div className="row">
-                <div className="col-sm">
-                    To do
-                 <ColumnTask tasks={tasks} status='todo'/>
+    return (
+        <div className="App">
+            <div className="container">
+                Kanban
+                <div className="row">
+                    <div className="col-sm">
+                        Todo
+                        <Board task={task} status={'progress'} leftArrow={leftArrow}/>
+                    </div>
+                    <div className="col-sm">
+                        Progress
+                        <Board task={task} status={'progress'} leftArrow={leftArrow}/>
+                    </div>
+                    <div className="col-sm">
+                        Review
+                        <Board task={task} status={'review'} leftArrow={leftArrow}/>
+                    </div>
+                    <div className="col-sm">
+                        Done
+                        <Board task={task} status={'done'} leftArrow={leftArrow}/>
+                    </div>
                 </div>
-                <div className="col-sm">
-                    Review
-                    <ColumnTask tasks={tasks}  status='review'/>
-                </div>
-                <div className="col-sm">
-                    Progress
-                    <ColumnTask tasks={tasks}  status='progress'/>
-                </div>
-                <div className="col-sm">
-                    Done
-                    <ColumnTask tasks={tasks} status='done'/>
                 </div>
             </div>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default App;
 
 
+44
