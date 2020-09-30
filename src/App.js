87809f4b7521b43a3bfba5571d4909cfd247ad2a
AlexeyIzhbulatov <ouren@mail.ru>
2020-09-30 13:02:39 -0700

    KanBan 1/4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import {v4 as uuidv4} from 'uuid';
 import Board from "./Board";
+import CreateFormTask from "./CreateFormTask";
 
 const initialTasks = [
     {id: uuidv4(), title: 'Task1', priority: 0, status: 'todo'},
@@ -15,30 +16,52 @@ function App() {
 
     const [task, setTask] = useState(initialTasks)
 
-    const leftArrow = (id, status) => {
-        console.log(id, status)
+
+
+    const updateArrowStatus = ({id, direction}) => {
+        console.log(id, direction)
+        const statuses = ['todo', 'progress', 'review', 'done']
+        const arrowLeftUpdate = task.map(el => {
+            if(el.id === id) {
+                if(direction === 'left'){
+                    el.status = statuses[statuses.indexOf(el.status) - 1];
+                }
+                if(direction === 'right'){
+                    el.status = statuses[statuses.indexOf(el.status) + 1];
+                }
+                return el;
+            } else return el;
+        });
+        setTask(arrowLeftUpdate)
+    }
+
+    const onCreateTaskForm = (tasks) => {
+        const updateOnCreateTaskForm = [...task]
+        updateOnCreateTaskForm.push({id: uuidv4(), title: tasks, status: 'todo'})
+        setTask(task)
     }
 
     return (
         <div className="App">
             <div className="container">
                 Kanban
+                <CreateFormTask onCreateTaskForm={onCreateTaskForm}/>
                 <div className="row">
                     <div className="col-sm">
                         Todo
-                        <Board task={task} status={'progress'} leftArrow={leftArrow}/>
+                        <Board task={task} status={'todo'} updateArrowStatus={updateArrowStatus}/>
                     </div>
                     <div className="col-sm">
                         Progress
-                        <Board task={task} status={'progress'} leftArrow={leftArrow}/>
+                        <Board task={task} status={'progress'} updateArrowStatus={updateArrowStatus}/>
                     </div>
                     <div className="col-sm">
                         Review
-                        <Board task={task} status={'review'} leftArrow={leftArrow}/>
+                        <Board task={task} status={'review'} updateArrowStatus={updateArrowStatus}/>
                     </div>
                     <div className="col-sm">
                         Done
-                        <Board task={task} status={'done'} leftArrow={leftArrow}/>
+                        <Board task={task} status={'done'} updateArrowStatus={updateArrowStatus}/>
                     </div>
                 </div>
                 </div>
@@ -47,6 +70,3 @@ function App() {
 }
 
 export default App;
-
-
-44
